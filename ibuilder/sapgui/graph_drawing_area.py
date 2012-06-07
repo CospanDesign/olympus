7f11494c2fe0507d5b3443e794cc0363b6e00057
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2012-06-07 12:25:15 -0400

    Fixed style for graph_drawing_area.py

--- ibuilder/sapgui/graph_drawing_area.py
+++ ibuilder/sapgui/graph_drawing_area.py
@@ -3,39 +3,35 @@ from gtk import gdk
 import math
 from types import *
 
-"""Custom Drawing Area"""
 class GraphDrawingArea(gtk.DrawingArea):
+  """Custom Drawing Area"""
   def __init__(self):
-    super (GraphDrawingArea, self).__init__()
+    '''Inits the graph drawing area by connecting the expose event correctly.'''
+    super(GraphDrawingArea, self).__init__()
 
-    self.connect ( "expose_event", self.do_expose_event )
+    self.connect("expose_event", self.do_expose_event)
     gobject.timeout_add(50, self.tick)
 
-
-  def tick (self):
+  def tick(self):
     if self.window is None:
       return True
 
-    self.alloc = self.get_allocation()
-    if self.alloc.width < 0:
+    alloc = self.get_allocation()
+    if alloc.width < 0 or alloc.height < 0:
       return True
 
-    if self.alloc.height < 0:
-      return True
-
-    rect = gtk.gdk.Rectangle (  0, \
-                  0, \
-                  self.alloc.width, \
-                  self.alloc.height )
-    self.window.invalidate_rect ( rect, True )
+    rect = gtk.gdk.Rectangle(0, 0, alloc.width, alloc.height)
+    self.window.invalidate_rect(rect, True)
 
     return True # Causes timeout to tick again
 
 
-  def do_expose_event( self, widget, event ):
+  def do_expose_event(self, widget, event):
+    '''Draws the graph area?'''
 #    print "expose event"
-    self.cr = self.window.cairo_create( )
-    self.draw( *self.window.get_size())
+    self.cr = self.window.cairo_create()
+    self.draw(*self.window.get_size())
 
-
-
+if __name__ == '__main__':
+  # TODO some test of some sort
+  print 'No test yet written!'
