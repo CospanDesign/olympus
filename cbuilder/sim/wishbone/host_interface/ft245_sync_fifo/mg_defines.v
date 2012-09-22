1ba26f54a6f62d4a431dfa4f98006daf59b7cd67
cospan <dave.mccoy@cospandesign.com>
2012-09-22 04:25:07 -0400

    updated all the mg_defines.v to confrom
    The mg_defines that were changed are part of old sim files and should be
    removed in the future

--- cbuilder/sim/wishbone/host_interface/ft245_sync_fifo/mg_defines.v
+++ cbuilder/sim/wishbone/host_interface/ft245_sync_fifo/mg_defines.v
@@ -23,22 +23,30 @@ SOFTWARE.
 */
 
 /*
-	11/12/2011
-		-Added NACK support
-			commands
-				COMMAND_NACK_TO_WR: set the NACK timeout
-				COMMAND_NACK_TO_RD: read the NACK timeout
+  09/21/2012
+    -changed the master command interface to use the address for single read
+    and write
+    -added core dump feature so users can find out the state of the master
+    before the system was reset
+  06/24/2012
+    -Added the Reset command that will reset the state machine from
+    the host interface
+  11/12/2011
+    -Added NACK support
+      commands
+        COMMAND_NACK_TO_WR: set the NACK timeout
+        COMMAND_NACK_TO_RD: read the NACK timeout
 
-			status codes
-				NACK_TIMEOUT: a NACK timeout occured
+      status codes
+        NACK_TIMEOUT: a NACK timeout occured
 
-			default values:
-				DEF_NACK_TIMEOUT: currently  set to 20 hex or 32 ticks
-	11/06/2011
-		-Added PERIPH_INTERRUPT to notify users of a peripheral 
-			slave interrupt
-		-Changed COMMAND_INTERRUPT to COMMAND_WR_INT_EN and
-		COMMAND_RD_INT_EN
+      default values:
+        DEF_NACK_TIMEOUT: currently  set to 20 hex or 32 ticks
+  11/06/2011
+    -Added PERIPH_INTERRUPT to notify users of a peripheral 
+      slave interrupt
+    -Changed COMMAND_INTERRUPT to COMMAND_WR_INT_EN and
+    COMMAND_RD_INT_EN
 */
 
 // defines for the miracle grow project
@@ -46,23 +54,30 @@ SOFTWARE.
 `ifndef __MG_DEFINES__
 `define __MG_DEFINES__
 
-`define COMMAND_PING 		32'h00000000
-`define COMMAND_WRITE 		32'h00000001
-`define COMMAND_READ		32'h00000002
-`define COMMAND_RW_FLAGS	32'h00000007
-`define COMMAND_WR_INT_EN	32'h00000008
-`define COMMAND_RD_INT_EN	32'h00000009
-`define COMMAND_NACK_TO_WR	32'h0000000A
-`define COMMAND_NACK_TO_RD	32'h0000000B
+`define COMMAND_PING        32'h00000000
+`define COMMAND_WRITE       32'h00000001
+`define COMMAND_READ        32'h00000002
+`define COMMAND_RESET       32'h00000003
+`define COMMAND_MASTER_ADDR 32'h00000004
+`define COMMAND_CORE_DUMP   32'h0000000F
+
+//master address space
+`define MADDR_WR_FLAGS      32'h00000000
+`define MADDR_RD_FLAGS      32'h00000001
+`define MADDR_WR_INT_EN     32'h00000002
+`define MADDR_RD_INT_EN     32'h00000003
+`define MADDR_NACK_TO_WR    32'h00000004
+`define MADDR_NACK_TO_RD    32'h00000005
+`define MADDR_CORE_DUMP     32'h00000006
 
 //conditions
-`define PERIPH_INTERRUPT	32'h10000000
-`define NACK_TIMEOUT		32'h20000000
+`define PERIPH_INTERRUPT    32'h00000001
+`define NACK_TIMEOUT        32'h00000002
 
 //flags
-`define FLAG_MEM_BUS		16'h0001
+`define FLAG_MEM_BUS        16'h0001
 
 //default variables
-`define DEF_NACK_TIMEOUT	32'h00000020
+`define DEF_NACK_TIMEOUT    32'h00000100
 
 `endif //__MG_DEFINES__
--- cbuilder/sim/wishbone/master/test_master_interconnect/mg_defines.v
+++ cbuilder/sim/wishbone/master/test_master_interconnect/mg_defines.v
@@ -22,20 +22,62 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+/*
+  09/21/2012
+    -changed the master command interface to use the address for single read
+    and write
+    -added core dump feature so users can find out the state of the master
+    before the system was reset
+  06/24/2012
+    -Added the Reset command that will reset the state machine from
+    the host interface
+  11/12/2011
+    -Added NACK support
+      commands
+        COMMAND_NACK_TO_WR: set the NACK timeout
+        COMMAND_NACK_TO_RD: read the NACK timeout
+
+      status codes
+        NACK_TIMEOUT: a NACK timeout occured
+
+      default values:
+        DEF_NACK_TIMEOUT: currently  set to 20 hex or 32 ticks
+  11/06/2011
+    -Added PERIPH_INTERRUPT to notify users of a peripheral 
+      slave interrupt
+    -Changed COMMAND_INTERRUPT to COMMAND_WR_INT_EN and
+    COMMAND_RD_INT_EN
+*/
 
 // defines for the miracle grow project
 
 `ifndef __MG_DEFINES__
 `define __MG_DEFINES__
 
-`define COMMAND_PING 		32'h00000000
-`define COMMAND_WRITE 		32'h00000001
-`define COMMAND_READ		32'h00000002
-`define COMMAND_WSTREAM_C 	32'h00000003
-`define COMMAND_WSTREAM		32'h00000004
-`define COMMAND_RSTREAM_C	32'h00000005
-`define COMMAND_RSTREAM		32'h00000006
-`define COMMAND_RW_FLAGS	32'h00000007
-`define COMMAND_INTERRUPT	32'h00000008
+`define COMMAND_PING        32'h00000000
+`define COMMAND_WRITE       32'h00000001
+`define COMMAND_READ        32'h00000002
+`define COMMAND_RESET       32'h00000003
+`define COMMAND_MASTER_ADDR 32'h00000004
+`define COMMAND_CORE_DUMP   32'h0000000F
+
+//master address space
+`define MADDR_WR_FLAGS      32'h00000000
+`define MADDR_RD_FLAGS      32'h00000001
+`define MADDR_WR_INT_EN     32'h00000002
+`define MADDR_RD_INT_EN     32'h00000003
+`define MADDR_NACK_TO_WR    32'h00000004
+`define MADDR_NACK_TO_RD    32'h00000005
+`define MADDR_CORE_DUMP     32'h00000006
+
+//conditions
+`define PERIPH_INTERRUPT    32'h00000001
+`define NACK_TIMEOUT        32'h00000002
+
+//flags
+`define FLAG_MEM_BUS        16'h0001
+
+//default variables
+`define DEF_NACK_TIMEOUT    32'h00000100
 
 `endif //__MG_DEFINES__
